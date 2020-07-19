18ae83f5962daef812e56e35dea19033a8cefa0c
Lucas <araujolucas97@gmail.com>
2020-07-18 22:53:37 -0300

    new

--- Project4/src/tests/createUser.ts
+++ Project4/src/tests/createUser.ts
@@ -1,4 +1,3 @@
-
 import request from 'supertest';
 import app from '../configs/expressConfig';
 import {createConnection} from 'typeorm';
@@ -17,4 +16,32 @@ describe("User", () => {
       expect(result.status).toBe(200);
 
    });
+
+   it("informei senha errada", async () => {
+
+      const result = await request(app)
+         .get("/users/console");
+
+      expect(result.status).toBe(400);
+      
+   });
+
+   it("informei senha certa", async () => {
+
+      const result = await request(app)
+         .get("/users/console");
+
+      expect(result.status).toBe(200);
+      
+   });
+
+   it("informei email e senha errados", async () => {
+
+      const result = await request(app)
+         .get("/users/console");
+
+      expect(result.status).toBe(200);
+      
+   });
+
 });
